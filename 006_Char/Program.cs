f371d2a9289efb93818e77a537d6c9cc7a84a7f8
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-07-03 15:50:53 +0300

    fix: add & finish Char project

---
+++ 006_Char/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _006_Char
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = 'A';
+            char b = '\x0041';
+            char c = '\u0041';
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+            Console.ReadLine();
+        }
+    }
+}
