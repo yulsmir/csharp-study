88bf20f19df8f3165dc9e7c8bd99ef8e149ee2b4
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-07-03 15:23:40 +0300

    fix: finish basic types

--- 003_BasicTypes/Program.cs
+++ 003_BasicTypes/Program.cs
@@ -36,6 +36,36 @@ namespace _003_BasicTypes
             Int64 p = +9223372036854775807L;
 
             #endregion
+
+            #region ВЕЩЕСТВЕННЫЕ ТИПЫ
+            float q = -0.123456789f;
+            Single r = +1.123456789F;
+
+            double s = -0.123456789d;
+            Double t = +1.123456789D;
+            #endregion
+
+            #region ДЕСЯТИЧНЫЙ ТИП
+            decimal u = -0.12345m;
+            Decimal v = +1.1234567M;
+            #endregion
+
+            #region ЛОГИЧЕСКИЙ ТИП
+            bool w = true;
+            Boolean x = false;
+            #endregion
+
+            #region СИМВОЛЬНЫЙ ТИП
+            char y = 'a';
+            Char z = 'b';
+            #endregion
+
+            #region СТРОКОВОЙ ТИП
+            string firstString = "hello";
+            String secondString = "HELLO";
+            #endregion
+
+            Console.ReadKey();
         }
     }
 }
