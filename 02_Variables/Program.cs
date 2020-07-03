fe4c218b26d65b5400cfca0bb7b8588f3e6cd819
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-07-03 12:41:46 +0300

    add: add project 2 - variables

---
+++ 02_Variables/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte a = 2;
+        }
+    }
+}
