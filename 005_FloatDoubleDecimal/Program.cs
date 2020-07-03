c35ee87d800aea1e77f4c16f7ab1750391e05981
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-07-03 15:46:41 +0300

    fix: finish FloatDoubleDecimal

--- 005_FloatDoubleDecimal/Program.cs
+++ 005_FloatDoubleDecimal/Program.cs
@@ -10,6 +10,15 @@ namespace _005_FloatDoubleDecimal
     {
         static void Main(string[] args)
         {
+            float variable1 = 0.12345678901234567890f;
+            double variable2 = 0.12345678901234567890d;
+            decimal variable3 = 0.12345678901234567890m;
+
+            Console.WriteLine(variable1);
+            Console.WriteLine(variable2);
+            Console.WriteLine(variable3);
+
+            Console.ReadKey();
         }
     }
 }
