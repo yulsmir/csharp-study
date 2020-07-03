218931f8fa8d1c16c6e1a7dc7f2c1244f5332d9c
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-07-03 15:43:05 +0300

    fix: finish default values

---
+++ 004_DefaultValues/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _004_DefaultValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte a = 2;
+            bool b;
+            char c;
+            string s;
+
+            Console.WriteLine(a);
+            Console.ReadLine();
+
+        }
+    }
+}
