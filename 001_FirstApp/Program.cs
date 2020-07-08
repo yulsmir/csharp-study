07ea5115c1c881487fb3d3cd4f41f461b309b7d5
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-07-08 22:35:12 +0300

    fix: edit firstApp project

---
+++ 001_FirstApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
