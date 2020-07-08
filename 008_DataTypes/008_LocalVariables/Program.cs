04dc4fc73ad879111cf298c9d40ba4b13604e92a
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-07-08 22:20:03 +0300

    add: local variables

---
+++ 008_DataTypes/008_LocalVariables/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _008_LocalVariables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                int a = 1;
+                Console.WriteLine(a);
+            }
+            
+            {
+                int a = 2;
+                Console.WriteLine(a);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
