340dda91ea60efe14c644ef6d5fb5a5c9ebbb015
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-07-08 23:24:42 +0300

    edit: add new At & ChechedAndUnchecked projects

---
+++ 008_DataTypes/009_At/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _009_At
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int @bool = 7; 
+            Console.WriteLine(@bool);
+
+            string @myVariable = "Hello";
+            Console.WriteLine(myVariable);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 008_DataTypes/010_CheckedAndUnchecked/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _010_CheckedAndUnchecked
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                sbyte a = 126;
+                sbyte b = 127;
+                checked
+                {
+                    a++;
+                    unchecked
+                    {
+                        b++;
+                    }
+                    a++; // Overflow
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
