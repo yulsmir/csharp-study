79560bac7b6a24c9eb7594caf164a6d10b23fc3a
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-07-08 16:36:56 +0300

    add: add assignment project

---
+++ 008_DataTypes/006_IncDec/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _006_IncDec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number1 = 0;
+            Console.WriteLine(number1++); // Постфиксный инкремент - сначала выводим на экран, потом увелиичваем на 1
+            Console.WriteLine(number1);
+            Console.WriteLine("---------");
+
+            byte number2 = 0;
+            Console.WriteLine(++number2);
+            Console.WriteLine("---------");
+
+            sbyte number3 = 0;
+            Console.WriteLine(number3--);
+            Console.WriteLine(number3);
+            Console.WriteLine("---------");
+
+            sbyte number4 = 0;
+            Console.WriteLine(--number4);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 008_DataTypes/007_Assignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _007_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int variable2 = 0;
+            variable2 = variable2 + 5;
+            variable2 += 5;
+
+            uint variable3 = 0;
+            variable3 = variable3 - 5;
+            variable3 -= 5;
+
+            long variable4 = 0;
+            variable4 = variable4 * 5;
+            variable4 *= 5;
+
+            ulong variable5 = 0;
+            variable5 = variable5 / 5;
+            variable5 /= 5;
+
+            long variable6 = 0;
+            variable6 = variable6 % 5;
+            variable6 %= 5;
+
+            Console.WriteLine(variable2);
+            Console.WriteLine(variable3);
+            Console.WriteLine(variable4);
+            Console.WriteLine(variable5);
+            Console.WriteLine(variable6);
+
+            Console.ReadKey();
+        }
+    }
+}
