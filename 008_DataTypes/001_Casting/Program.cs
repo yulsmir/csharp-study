0b992fa3f48fc56a5dacb90fa87872501a5d9e87
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-07-08 14:56:09 +0300

    init: add 008_project Data Types

---
+++ 008_DataTypes/001_Casting/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_Casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
