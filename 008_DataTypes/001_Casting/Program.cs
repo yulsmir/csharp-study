184a266e517aecdc7e695daf81f4057fc4e019e7
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-07-08 15:26:46 +0300

    end: finish Casting project

--- 008_DataTypes/001_Casting/Program.cs
+++ 008_DataTypes/001_Casting/Program.cs
@@ -10,7 +10,38 @@ namespace _001_Casting
     {
         static void Main(string[] args)
         {
+            byte a = 10;
+            int b = 0;
+            b = a;
+            Console.WriteLine(b);
 
+            int c = 255;
+            float d = 0f;
+            d = c;
+            Console.WriteLine(d);
+
+            int e = 256;
+            byte f = 0;
+            f = (byte)e;
+            Console.WriteLine(f);
+
+            float g = 10.5F;
+            int h = 0;
+            h = (int)g;
+            Console.WriteLine(h);
+
+            const int i = 255;
+            byte j = 0;
+            j = i;
+            Console.WriteLine(j);
+
+            const float k = 255;
+            byte l = 0;
+            l = (byte)k;
+
+            Console.WriteLine(l);
+
+            Console.ReadKey();
         }
     }
 }
