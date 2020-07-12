4ac69d6cdfa77c7e84830d2c1d7fb318edba9271
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-07-12 23:05:30 +0300

    edit: add Comparison

---
+++ 008_DataTypes/016_Comparison/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _016_Comparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool result = false;
+
+            int a = 1;
+            float b = 2.0f;
+            result = a < b;
+
+            string c = "Hello";
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+    }
+}
