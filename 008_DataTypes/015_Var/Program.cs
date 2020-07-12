364ea52182d35da740475c3e864eed172b469d5d
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-07-12 22:49:49 +0300

    edit: add Var

---
+++ 008_DataTypes/015_Var/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _015_Var
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myVar = 7.3f;
+            Console.WriteLine(myVar);
+
+            Console.ReadKey();
+        }
+    }
+}
