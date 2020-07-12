f130ad5094263067c71e81bc9e3ea9ec6fa59c35
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-07-13 02:31:31 +0300

    add: add Conditions: -> If-Else

---
+++ 004_Conditions/003_If-else/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003_If_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2, b = 2;
+            if (a < b)
+            {
+                Console.WriteLine("a < b");
+            }
+
+            else if (a > b)
+            {
+                Console.WriteLine("a > b");
+            }
+
+            else
+            {
+                Console.WriteLine("a == b");
+            }
+            Console.ReadKey();
+        }
+    }
+}
