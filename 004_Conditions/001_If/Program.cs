353ff6afa69875d5c748109026728c3408fc8969
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-07-13 02:14:20 +0300

    add: Conditions -> If

---
+++ 004_Conditions/001_If/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_If
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1, b = 2;
+            if (a < b)
+            {
+                Console.WriteLine("a < b");
+                Console.ReadKey();
+            }
+        }
+    }
+}
