f9f448b8d9cdf95f139d70cf9d42e328883868b8
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-07-12 22:32:32 +0300

    edit: add sizeOf

---
+++ 008_DataTypes/014_SizeOf/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _014_SizeOf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int doubleSize = sizeof(double);
+            Console.WriteLine("Размер типа double: {0} байт.", doubleSize);
+
+            Console.WriteLine("Размер типа int: {0} байт.", sizeof(int));
+            Console.WriteLine("Размер типа bool: {0} байт.", sizeof(bool));
+            Console.WriteLine("Размер типа long: {0} байт.", sizeof(long));
+            Console.WriteLine("Размер типа short: {0} байт.", sizeof(short));
+
+            Console.ReadKey();
+        }
+    }
+}
