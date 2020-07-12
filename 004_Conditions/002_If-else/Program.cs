00ef7dad4fb85a2734741905326d32dde29c78b0
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-07-13 02:17:43 +0300

    add: Conditions -> If-Else

---
+++ 004_Conditions/002_If-else/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_If_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 11, b = 2;
+            if (a < b)
+            {
+                Console.WriteLine("a < b");
+            }
+            else 
+            {
+                Console.WriteLine("a is not less than b");
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
