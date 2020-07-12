1ab09c02432b1ab629137e4b4b204d7b497100d1
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-07-12 23:31:18 +0300

    add: radius count

---
+++ Homeworks/003_Variables/003_ RadiusCount/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003__RadiusCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.14;
+            double r = 2.56;
+
+            double square = pi * (Math.Pow(r, 2));
+            Console.WriteLine(square);
+            Console.ReadKey();
+        }
+    }
+}
