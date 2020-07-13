6b1fabb757a28d3d443957bf2280a2bfaff34b99
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-07-13 10:56:41 +0300

    add: add Conditions -> Authentication

---
+++ 004_Conditions/004_Authentication/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _004_Authentication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string login = "Admin";
+            string password = "P@ssw0rd";
+
+            Console.Write("Enter login: ");
+
+            string usersLogin = Console.ReadLine();
+            if (login == usersLogin)
+            {
+                Console.Write("Enter Password: ");
+                string usersPassword = Console.ReadLine();
+
+                if (password == usersPassword)
+                {
+                    Console.WriteLine("Hello {0}, you are in the system.", usersLogin);
+                }
+                else
+                {
+                    Console.WriteLine("You entered wrong password.");
+                }
+
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
