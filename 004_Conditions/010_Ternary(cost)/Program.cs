061d1c7923ee4af7a82d3659dec29ae2b4316ee3
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-07-20 15:31:06 +0200

    add: Ternary(Cost)

---
+++ 004_Conditions/010_Ternary(cost)/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _010_Ternary_cost_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double quantity = 10;
+            double price = 100;
+            double discount = 0.75;
+            double cost;
+
+            cost = quantity >= 10 ? quantity * price * discount : quantity * price;
+
+            Console.WriteLine("Total cost: {0} usd", cost);
+            Console.ReadKey();
+
+        }
+    }
+}
