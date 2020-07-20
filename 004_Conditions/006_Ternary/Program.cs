7af369731ab050075fbb18d303f40d74cf82e237
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-07-20 14:36:06 +0200

    add: 007_Ternary

---
+++ 004_Conditions/006_Ternary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _006_Ternary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1, b = 2, c = -5, max = 0;
+            max = a > b ? c = a : c = b;
+            Console.WriteLine(max);
+            Console.WriteLine(c);
+
+            Console.ReadKey();
+        }
+    }
+}
