dd25c00aaa0bd7eae979227835c6fb1612664b34
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-07-20 15:14:27 +0200

    add: Ternary(types)

---
+++ 004_Conditions/008_Ternary(Types)/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _008_Ternary_Types_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+            double b = 0.0;
+            int max;
+            max = (int)((a > b) ? a : b);
+
+            max = (a > b) ? a : (int)b;
+            Console.WriteLine(max);
+            Console.ReadKey();
+        }
+    }
+}
