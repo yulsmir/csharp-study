85cba16d8fa550b700930c33afa4b92a313d6980
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-07-20 14:45:04 +0200

    add: ternary string - login

---
+++ 004_Conditions/007_Ternary(String)/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _007_Ternary_String_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greetingString = "Hello ";
+            Console.WriteLine("Enter Your Login:");
+
+            string login = Console.ReadLine();
+
+            greetingString += login == "Admin" ? "Administrator" : "User";
+            Console.WriteLine(greetingString);
+            
+            Console.ReadKey();
+        }
+    }
+}
