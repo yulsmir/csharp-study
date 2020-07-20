46b0f82b8f52da93612b9186ad023eae5ddf9244
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-07-20 15:26:38 +0200

    add: Ternary(Coordinates)

---
+++ 004_Conditions/009_Ternary(Coordinates)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _009_Ternary_Coordinates_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = -6, y = -5;
+
+            string quadrant = (x > 0) ? ((y > 0) ? "I quadrant" : "IV quadrant") : ((y > 0) ? "II quadrant" : "III quadrant");
+
+            //or
+
+            quadrant = x > 0 ? (y > 0 ? "I quadrant" : "IV quadrant") : (y > 0 ? "II quadrant" : "III quadrant");
+
+            //or
+            quadrant = x > 0 ? y > 0 ? "I quadrant" : "IV quadrant" : y > 0 ? "II quadrant" : "III quadrant";
+
+            Console.WriteLine(quadrant);
+            Console.ReadKey();
+        }
+    }
+}
