165dc7d0d5c35fab40fc0e130b2abd14daa30faf
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-07-21 18:25:52 +0200

    add: switch case (multiple)

---
+++ 004_Conditions/013_Switch/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _013_Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the weekday number: 1-2-3-4-5-6-7: ");
+
+            string day = Console.ReadLine();
+            switch (day)
+            {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                    Console.WriteLine("This weekday is a working day.");
+                    break;
+                case "6":
+                case "7":
+                    Console.WriteLine("This weekday is a weekend.");
+                    break;
+
+                default:
+                    Console.WriteLine("The day you entered doesn't exist.");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
