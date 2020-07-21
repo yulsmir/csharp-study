f7be298f4da50f6c2044c82e542cf69b2a076947
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-07-21 18:37:43 +0200

    add: coffee maker

---
+++ 004_Conditions/014_CoffeeMaker/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _014_CoffeeMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome! We have the best coffee in the world!");
+            Console.WriteLine("Coffee cup sizes: 1 = small, 2 = medium, 3 = large");
+            Console.WriteLine("Please, select your cup size: ");
+
+            string coffeeSize = Console.ReadLine();
+
+            int cost = 0;
+
+            switch (coffeeSize)
+            {
+                case "1":
+                    cost += 25;
+                    break;
+                case "2":
+                    cost += 50;
+                    break;
+                case "3":
+                    cost += 75;
+                    break;
+
+                default:
+                    Console.WriteLine("Incorrect choice. Please, choose 1, 2 or 3.");
+                    break;
+            }
+            if (cost != 0)
+            {
+                Console.WriteLine("Enter {0} cents", cost);
+                Console.WriteLine("Thank you!");
+            }
+
+            else
+            {
+                Console.WriteLine("Please, repeat your choice.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
