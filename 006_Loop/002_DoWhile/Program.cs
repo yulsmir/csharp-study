ee9406a4b4f5cec5d0844c4b40471728635badcd
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-08-02 22:09:29 +0200

    add: dowhile counter + dowhile factorial

---
+++ 006_Loop/002_DoWhile/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0;
+            do
+            {
+                counter++;
+                Console.WriteLine("Counter {0}", counter);
+            }
+            while (counter < 3);
+            Console.WriteLine("{0} iterations made.", counter);
+            Console.ReadKey();
+        }
+    }
+}
