99999cba82588cd92a7b3488552974e50c415800
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-08-02 22:27:42 +0200

    add: for counter

---
+++ 006_Loop/004_ForCounter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _004_ForCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int counter = 0; counter < 3; counter++)
+            {
+                Console.WriteLine("Counter = {0}", counter);
+            }
+            Console.ReadLine();
+        }
+    }
+}
