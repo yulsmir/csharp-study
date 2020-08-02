93842470238d766cd82fdd99694e4f42a3002aa7
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-08-02 21:35:36 +0200

    add: loop - GuessColor

---
+++ 006_Loop/001_GuessColor/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_GuessColor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess the color. You have 5 tries.");
+            Console.WriteLine("To finish program type - exit.");
+
+            const int maxAttempt = 5;
+            int attempt = 0;
+            string color = "red";
+
+            while (attempt < maxAttempt)
+            {
+                attempt++;
+                Console.WriteLine("Try {0}:", attempt);
+
+                string value = Console.ReadLine();
+                if (value == "exit")
+                {
+                    break;
+
+                }
+                if (value != color)
+                {
+                    continue;
+                }
+
+                Console.WriteLine("Greetings! You guessed from the {0} try!", attempt);
+                break;
+            }
+            Console.WriteLine("End of the game!");
+            Console.ReadKey();
+        }
+    }
+}
