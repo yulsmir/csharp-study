3574af1dff77d3d7e24c8e11203bbac3a1dca75a
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-08-03 23:45:17 +0200

    add: method - draw rectangle

---
+++ 007_Method/002_DrawRectangleMethod/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_DrawRectangleMethod
+{
+    class Program
+    {
+        static void DrawRectangle(int height, int width)
+        {
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+        static void Main()
+        {
+            DrawRectangle(10, 4);
+            DrawRectangle(6, 14);
+            Console.ReadKey();
+        }
+    }
+}
