f2a38d73389ae4d19cf8acd3bc8cab168dee3cf8
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-08-04 00:18:20 +0200

    add: method - summary

---
+++ 007_Method/004_SumMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _004_SumMethod
+{
+    class Program
+    {
+
+        static int Sum(int summand1, int summand2)
+        {
+            return summand1 + summand2;
+        }
+
+        static void Main()
+        {
+            int summand1 = 10, summand2 = 25;
+            int sum = Sum(summand1, summand2);
+            Console.WriteLine("{0} + {1} = {2}", summand1, summand2, sum);
+
+            Console.ReadKey();
+        }
+    }
+}
