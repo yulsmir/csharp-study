5570a89cb2ca2b5d3edec2d4d9ce3aba15075672
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-08-04 00:00:33 +0200

    add: method - procedure

---
+++ 007_Method/003_Methods/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003_Methods
+{
+    class Program
+    {
+        static void Procedure()
+        {
+            Console.WriteLine("Hello!");
+        }
+        static void Main()
+        {
+            Procedure();
+            Console.ReadKey();
+        }
+    }
+}
