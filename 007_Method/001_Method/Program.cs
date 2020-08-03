ab8e2ef291e7033b1d4e0102751260698262e0d2
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-08-03 22:48:15 +0200

    add: first method

---
+++ 007_Method/001_Method/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_Method
+{
+    class Program
+    {
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        static void Main()
+        {
+            int sum = Add(2, 3);
+            Console.WriteLine(sum);
+
+            Console.ReadKey();
+        }
+    }
+}
