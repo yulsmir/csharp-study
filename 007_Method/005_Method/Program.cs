b4b8b8056736bbedc1469f08395aec6f444d9fed
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-08-06 22:01:51 +0200

    add: bool summary method

---
+++ 007_Method/005_Method/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _005_Method
+{
+    class Program
+    {
+        static bool And(bool a, bool b)
+        {
+            return a && b;
+        }
+        static void Main(string[] args)
+        {
+            bool operand1 = true, operand2 = true;
+            bool result = And(operand1, operand2);
+            Console.WriteLine("{0} && {1} = {2}", operand1, operand2, result);
+            Console.ReadKey();
+        }
+    }
+}
