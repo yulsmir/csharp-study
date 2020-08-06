606cd413a8de54e8cbacb90fb0451b4be46c283f
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-08-07 00:23:33 +0200

    add: power method

---
+++ 007_Method/Test - Times/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test___Times
+{
+    class Program
+    {
+        static ulong power(uint x, uint n)
+        {
+            ulong result = x;
+
+            for (int i = 1; i < n; i++)
+            {
+                result *= x;
+            }
+            if (n == 0)
+            {
+                return 1;
+            }
+            return result;
+        }
+        static void Main()
+        {
+            uint operand1 = 5;
+            uint operand2 = 2;
+            ulong result = power(operand1, operand2);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
