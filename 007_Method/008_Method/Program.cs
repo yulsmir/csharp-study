c9efe74aa59c74b0b9fd6f0f8055ff2c3c5c8000
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-08-06 22:45:32 +0200

    edit: fix method

--- 007_Method/008_Method/Program.cs
+++ 007_Method/008_Method/Program.cs
@@ -7,10 +7,21 @@ using System.Threading.Tasks;
 namespace _008_Method
 {
     //Ref = in/out
+    
     class Program
     {
-        static void Main(string[] args)
+        static int Method(ref int a)
         {
+            int b = a * 2;
+            a = 10;
+            return b;
+        }
+        static void Main()
+        {
+            int operand = 25;
+            int result = Method(ref operand);
+            Console.WriteLine("{0}; {1};", operand, result);
+            Console.ReadKey();
         }
     }
 }
