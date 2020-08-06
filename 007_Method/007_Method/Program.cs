4e41bd1e7f7271269ff0d03726825791e0388a54
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-08-06 22:21:48 +0200

    add: method name check

---
+++ 007_Method/007_Method/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _007_Method
+{
+    class Program
+    {
+        static string Function(string name)
+        {
+            if (name == "fool")
+            {
+                return "You used inappropriate word";
+            }
+            string sentence = "Hello " + name + "!";
+            return sentence;
+        }
+        static void Main()
+        {
+            string sentence = Function("fool");
+            Console.WriteLine(sentence);
+            Console.ReadKey();
+        }
+    }
+}
