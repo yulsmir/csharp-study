61bf80a795586137fa5bc859eb8f164440f269aa
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-08-06 23:08:12 +0200

    fix: edit method with out parameter

--- 007_Method/009_Method/Program.cs
+++ 007_Method/009_Method/Program.cs
@@ -8,8 +8,17 @@ namespace _009_Method
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Method(out int a)
         {
+            a = 2;
+            return a * 2;
+        }
+        static void Main()
+        {
+            int operand;
+            int result = Method(out operand);
+            Console.WriteLine("{0}; {1};", operand, result);
+            Console.ReadKey();
         }
     }
 }
