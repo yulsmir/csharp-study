f46e06c0686e15145d35e3136157bae0dd6dd3c6
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-08-06 22:30:24 +0200

    add: methods with reference (in/out)

---
+++ 007_Method/008_Method/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _008_Method
+{
+    //Ref = in/out
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
