ba5039e4c54602722b94217b48ef80503fad001b
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-08-09 21:28:50 +0200

    add: method add two

--- 007_Method/010_Method/Program.cs
+++ 007_Method/010_Method/Program.cs
@@ -8,8 +8,23 @@ namespace _010_Method
 {
     class Program
     {
-        static void Main(string[] args)
+        static void AddTwo(int a)
         {
+            a = a + 2;
+            Console.WriteLine("Value of int a = {0}", a);
+        }
+        static void Main()
+        {
+            Console.WriteLine("Enter the number");
+            string number = Console.ReadLine();
+            int result = Int32.Parse(number);
+
+            Console.WriteLine("Value result = {0}", result);
+
+            AddTwo(result);
+            Console.WriteLine("Value of result = {0}", result);
+
+            Console.ReadKey();
         }
     }
 }
