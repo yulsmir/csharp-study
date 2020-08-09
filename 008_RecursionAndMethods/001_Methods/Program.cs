60f0a580cc8e6d5aa7e8afae7404173241b57755
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-08-09 22:53:35 +0200

    add: named method difference

---
+++ 008_RecursionAndMethods/001_Methods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Methods overload
+namespace _001_Methods
+{
+    class Program
+    {
+        static int Difference(int height, int width)
+        {
+            return height - width;
+        }
+        static void Main()
+        {
+            int difference = Difference(6, 5);
+            Console.WriteLine("Difference equals: {0}", difference);
+
+            //or
+
+            difference = Difference(width: 5, height: 6);
+            Console.WriteLine("Difference equals: {0}", difference);
+            Console.ReadKey();
+        }
+    }
+}
