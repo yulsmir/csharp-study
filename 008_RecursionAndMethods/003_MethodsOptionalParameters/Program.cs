3337457d1d582f4c419e7e09a2b7be5437349a68
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-08-09 23:39:51 +0200

    fix: edit optional method

---
+++ 008_RecursionAndMethods/003_MethodsOptionalParameters/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003_MethodsOptionalParameters
+{
+    class Program
+    {
+        static void Operation(string value1 = "val", int value2 = 10, double value3 = 12.2)
+        {
+            Console.WriteLine("{0}, {1}, {2}", value1, value2, value3);
+        }
+        static void Main()
+        {
+            Operation();
+            Operation("val");
+            Operation("val", 10);
+            Operation("val", 10, 12.2);
+
+            //Operation("Alex", (int)12.2);
+
+            Operation("val", value3: 12.2);
+            Operation(value2: 33, value3: 12.2);
+
+            Console.ReadKey();
+        }
+    }
+}
