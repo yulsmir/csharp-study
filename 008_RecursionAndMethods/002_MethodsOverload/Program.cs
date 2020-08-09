a927775f708a98552472d67b3423a48da2ea5f82
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-08-09 23:16:46 +0200

    add: method with overload

---
+++ 008_RecursionAndMethods/002_MethodsOverload/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_MethodsOverload
+{
+    class Program
+    {
+        static void Operation()
+        {
+            Operation("val", 10, 12.2);
+        }
+        static void Operation(string value1)
+        {
+            Operation(value1, 10, 12.2);
+        }
+
+        static void Operation(string value1, int value2)
+        {
+            Operation(value1, value2, 12.2);
+        }
+
+        static void Operation(string value1, int value2, double value3)
+        {
+            Console.WriteLine("{0},{1},{2}", value1, value2, value3);
+        }
+        static void Main()
+        {
+            Operation();
+            Operation("val");
+            Operation("val", 10);
+            Operation("val", 10, 12.2);
+            Console.ReadKey();
+        }
+    }
+}
