cc9685372bc06fbc9607983917694bfc4d83a0d8
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-08-11 00:00:06 +0200

    add: two dimensional array 3x3

---
+++ 009_Arrays/005_ArrayTwoDimensional/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _005_ArrayTwoDimensional
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[,] array = {
+                             { 1, 2, 3 },
+                             { 4, 5, 6 },
+                             { 7, 8, 9 },
+                           };
+
+            Console.WriteLine(array);
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write("{0} ", array[i, j]);
+                }
+                Console.Write("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
