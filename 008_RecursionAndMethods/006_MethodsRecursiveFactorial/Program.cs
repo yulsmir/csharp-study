e59291d258f060b1135ae1912dcaad0e9fca3831
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-08-10 20:57:43 +0200

    add: factorial count

---
+++ 008_RecursionAndMethods/006_MethodsRecursiveFactorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _006_MethodsRecursiveFactorial
+{
+    class Program
+    {
+        static int Factorial(int n)
+        {
+            if (n == 0)
+                return 1;
+            else
+                return n * Factorial(n - 1);
+        } 
+         
+        static void Main()
+        {
+            int factorial = Factorial(5);
+            Console.WriteLine(factorial);
+            Console.ReadKey();
+        }
+    }
+}
