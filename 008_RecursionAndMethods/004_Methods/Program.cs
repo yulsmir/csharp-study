9e61199f1881cfd223e9b2415783223cf1b50503
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-08-10 19:28:19 +0200

    add: method with recursion

---
+++ 008_RecursionAndMethods/004_Methods/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _004_Methods
+{
+    class Program
+    {
+        static void Recursion(int counter)
+        {
+            Console.WriteLine("First part of method Recursion: {0}", counter);
+            if (counter != 0)
+            {
+                Method(counter);
+            }
+        }
+        static void Method(int counter)
+        {
+            Console.WriteLine("First part of method Recursion: {0}", counter);
+            Recursion(counter);
+            Console.WriteLine("Second part of method Recursion: {0}", counter);
+        }
+
+        static void Main()
+        {
+            Method(3);
+            Console.ReadKey();
+        }
+    }
+}
