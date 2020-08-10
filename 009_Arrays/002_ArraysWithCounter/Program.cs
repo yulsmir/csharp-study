f4a2ad0c1792124d17bc62383b87eb9e383701f3
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-08-10 22:17:10 +0200

    fix: edit array counter

---
+++ 009_Arrays/002_ArraysWithCounter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_ArraysWithCounter
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] array = new int[5];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i * 2;
+                Console.WriteLine(array[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
