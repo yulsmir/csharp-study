5acc266b5bf8f7b0fedc546bdb4b6bb2d69b9a33
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-08-10 22:52:50 +0200

    add: arrays

---
+++ 009_Arrays/003_Arrays/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _003_Arrays
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] array = new int[5] { 1, 2, 3, 4, 5 };
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
