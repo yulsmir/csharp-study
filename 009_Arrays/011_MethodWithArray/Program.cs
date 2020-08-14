b06a81c1509eedaf50539b334cc1abbf2d39b472
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-08-14 05:40:36 +0200

    edit: fix method with array (add mdifier)

--- 009_Arrays/011_MethodWithArray/Program.cs
+++ 009_Arrays/011_MethodWithArray/Program.cs
@@ -8,8 +8,25 @@ namespace _011_MethodWithArray
 {
     class Program
     {
-        static void Main(string[] args)
+        static int[] ModifyArray(int[] array, int modifier)
         {
+            for (int i = 0; i < array.Length; i++)
+			{
+                array[i] = array[i] * modifier;
+			}
+            return array;
+        }
+        static void Main()
+        {
+            int[] myArray = { 1, 2, 3, 4, 5 };
+            myArray = ModifyArray(myArray, 5);
+
+            for (int i = 0; i < myArray.Length; i++)
+			{
+                Console.Write("{0}, ", myArray[i]);
+			}
+
+            Console.ReadKey();
         }
     }
 }
