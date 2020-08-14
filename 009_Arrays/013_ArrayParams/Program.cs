be75c11c0d9af4267118a0dfc1c7a241396354a1
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-08-14 05:14:48 +0200

    add: array with params(string and array)

---
+++ 009_Arrays/013_ArrayParams/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _013_ArrayParams
+{
+    class Program
+    {
+        static void ShowArray(string name, params int[] array)
+        {
+            Console.Write(name);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("{0} ", array[i]);
+            }
+        }
+        static void Main()
+        {
+            ShowArray("Numbers: ", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+
+            Console.ReadKey();
+        }
+    }
+}
