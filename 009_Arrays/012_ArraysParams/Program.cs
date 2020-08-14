c9b9abb1881ea3e73d00fe826b4e9389078f76c4
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-08-14 05:06:33 +0200

    add: method witharray & array with params

---
+++ 009_Arrays/012_ArraysParams/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _012_ArraysParams
+{
+    class Program
+    {
+        static void ShowArray(params int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write("{0} ", array[i]);
+            }
+        }
+        static void Main()
+        {
+            ShowArray(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0);
+            Console.ReadKey();
+        }
+    }
+}
