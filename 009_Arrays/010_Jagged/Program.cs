172034ace06441c94beab7012ef677a5b4bb17c0
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-08-14 04:19:41 +0200

    add: jagged array v2

---
+++ 009_Arrays/010_Jagged/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _010_Jagged
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[][] jagged = new[]
+            {
+                new int [] { 1, 2 },
+                new int [] { 1, 2, 3, 4, 5, 6 },
+                new int [] { 1, 2, 3 }
+            };
+            for (int i = 0; i < jagged.Length; ++i)
+            {
+                for (int j = 0; j < jagged[i].Length; j++)
+                {
+                    Console.Write("{0} ", jagged[i][j]);
+                }
+                Console.Write("\n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
