8c5d2e2f78bf9872f02741d036ffafe7d5e6ecad
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-08-14 02:24:50 +0200

    add: one element array (matrix, vector)

---
+++ 009_Arrays/007_Vector/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _007_Vector
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] vector = new int[1];
+            vector[0] = 100;
+            Console.WriteLine(vector[0]);
+
+            int[,] matrix = new int[1, 1];
+            matrix[0, 0] = 200;
+            Console.WriteLine(matrix[0, 0]);
+
+            Console.ReadKey();
+        }
+    }
+}
