3abb438c313e9e88e35875a75999027fbf6370af
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-08-16 01:02:36 +0200

    add: multiple startup project

---
+++ Practice/003_ConsoleColor/01_Program/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("Hello");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Practice/003_ConsoleColor/02_Program/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine("world");
+            Console.ReadKey();
+        }
+    }
+}
