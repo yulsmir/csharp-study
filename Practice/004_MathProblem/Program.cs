5647872c55a2b46e2a89c08d611dfc3233fbd2df
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-08-16 03:35:47 +0200

    add: fruit sum count

---
+++ Practice/004_MathProblem/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _004_MathProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int apples = 8;
+            int pears = 5;
+
+            int fruitSum = apples + pears;
+            Console.WriteLine(fruitSum);
+
+            Console.ReadKey();
+        }
+    }
+}
