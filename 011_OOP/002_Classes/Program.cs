69df1ebeb56f4cafa4aa7f16111412577d00bf37
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-08-18 23:50:11 +0200

    add: classes

---
+++ 011_OOP/002_Classes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_Classes
+{
+    class MyClass
+    {
+        public string field;
+        public void Method()
+        {
+            Console.WriteLine(field);
+        }
+    }
+    class Program
+    {
+        
+        static void Main()
+        {
+            MyClass instance = new MyClass();
+            instance.field = "Hello world!";
+            Console.WriteLine(instance.field);
+            instance.Method();
+
+            Console.ReadKey();
+        }
+    }
+}
