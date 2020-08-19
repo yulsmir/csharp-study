a7f9cab3b8d4358e74ccdf989e7c380b7ee3283e
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-08-19 23:56:00 +0200

    add: class with setters and getters

--- 011_OOP/003_Classes/Program.cs
+++ 011_OOP/003_Classes/Program.cs
@@ -6,10 +6,30 @@ using System.Threading.Tasks;
 
 namespace _003_Classes
 {
+    class MyClass
+    {
+        private string field = null;
+        public void SetField(string value)
+        {
+            field = value;
+        }
+
+        public string GetField()
+        {
+            return field;
+        }
+    }
     class Program
     {
-        static void Main(string[] args)
+        
+        static void Main()
         {
+            MyClass instance = new MyClass();
+            instance.SetField("Jonny");
+            string @string = instance.GetField();
+            Console.WriteLine(@string);
+
+            Console.ReadKey();
         }
     }
 }
