f5eaf11a94c92293ec86cfd4980a19ba20cdbe14
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-09-04 15:11:18 +0200

    add: oop + first constructor

--- 011_OOP/005_Constructor/Program.cs
+++ 011_OOP/005_Constructor/Program.cs
@@ -4,15 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _004_Project
+namespace _005_Constructor
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(new string('-', 50));
-            Console.WriteLine("Hello Wolrd!");
-            Console.ReadKey();
         }
     }
 }
