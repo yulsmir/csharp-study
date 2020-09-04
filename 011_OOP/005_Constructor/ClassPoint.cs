55563bd678e57ff2b4b804c20281102070823be6
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-09-04 15:14:08 +0200

    edit: refactor code

--- 011_OOP/005_Constructor/ClassPoint.cs
+++ 011_OOP/005_Constructor/ClassPoint.cs
@@ -1,1 +1,29 @@
-﻿
+﻿using System;
+
+namespace Classes
+{
+    class Point
+    {
+        private int x, y;
+        public int X
+        {
+            get { return x; }
+        }
+        public int Y
+        {
+            get { return y; }
+        }
+
+        public Point()
+        {
+            Console.WriteLine("Default constructor");
+        }
+
+        public Point(int x, int y)
+        {
+            Console.WriteLine("User constructor");
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
