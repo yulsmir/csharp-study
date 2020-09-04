3f7ad92ba7b155e6e67e7ef56a05266de364a74a
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-09-04 18:25:54 +0200

    add: classes + extratask

---
+++ 011_OOP/008_Classes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _008_Classes
+{
+    class MyClass
+    {
+        public void Method()
+        {
+            Console.WriteLine("Welcome onboard!");
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            new MyClass().Method();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 011_OOP/ExtraTask/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtraTask
+{
+    class Program
+    {
+        class Address
+        {
+            public int index, house, apartment;
+            public string country, city, street;
+
+            public string Country { get; set; }
+            public string City { get; set; }
+            public string Street { get; set; }
+            public int Index { get; set; }
+            public int House { get; set; }
+            public int Apartment { get; set; }
+        }
+        static void Main()
+        {
+            Address address1 = new Address()
+            {
+                Index = 12345,
+                Country = "Ghana",
+                City = "Zimba",
+                Street = "Wallstreet",
+                House = 23,
+                Apartment = 1
+            };
+
+            Console.WriteLine("Index: {0}, Country: {1}, City: {2}, Street: {3}, House: {4}, Apartment: {5}", address1.Index, address1.Country, address1.City, address1.Street, address1.House, address1.Apartment);
+            
+            Console.ReadKey();
+        }
+    }
+}
