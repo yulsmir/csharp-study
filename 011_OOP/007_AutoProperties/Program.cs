bc139d8e1074f6b79ac8929c9c194669c1d5f518
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-09-04 17:48:50 +0200

    add: auto-implemented properties

---
+++ 011_OOP/007_AutoProperties/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _007_AutoProperties
+{
+    class Program
+    {
+        public class Author
+        {
+            public string Name { get; set; }
+            public string Book { get; set; }
+        }
+
+        static void Main()
+        {
+            Author author1 = new Author()
+            {
+                Name = "Jeffrey Richter",
+                Book = "CLR via C#"
+            };
+
+            Author author2 = new Author()
+            {
+                Name = "Steve McConnel",
+                Book = "Code Complete"
+            };
+
+            Console.WriteLine("Name: {0}, Book: {1}", author1.Name, author1.Book);
+            Console.WriteLine(new string('-', 50));
+            Console.WriteLine("Name: {0}, Book: {1}", author2.Name, author2.Book);
+
+            Console.ReadKey();
+        }
+    }
+}
