28a7d2b95b11cc7e97cccab164f6c4e947064962
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-09-04 17:37:01 +0200

    add: constructor

--- 011_OOP/005_Constructor/Program.cs
+++ 011_OOP/005_Constructor/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,16 @@ namespace _005_Constructor
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Point pointA = new Point();
+            Console.WriteLine("pointA.X = {0} pointA.Y = {1}", pointA.X, pointA.Y);
+            Console.WriteLine(new string('-', 30));
+
+            Point pointB = new Point();
+            Console.WriteLine("pointB.X = {0} pointB.Y = {1}", pointB.X, pointB.Y);
+
+            Console.ReadKey();
         }
     }
 }
---
+++ 011_OOP/006_Classes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _006_Classes
+{
+    class MyClass
+    {
+        public void Method()
+        {
+            Console.WriteLine("MyClass' method is called");
+        }
+    }
+
+    class MyClass2
+    {
+        public void CallMethod(MyClass my)
+        {
+            my.Method();
+        }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            MyClass my = new MyClass();
+            MyClass2 my2 = new MyClass2();
+
+            my2.CallMethod(my);
+            Console.ReadKey();
+        }
+    }
+}
