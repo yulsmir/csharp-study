6d75012271cf63f92050ea023b3cdaa65bcaed2c
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-09-09 16:21:09 +0200

    add: homework triangle perimeter

---
+++ 011_OOP/Homework_TrianglePerimeter/Point.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_TrianglePerimeter
+{
+    class Point
+    {
+        int a;
+        int b;
+        string name;
+
+        public int A { get; }
+        public int B { get; }
+        public string Name { get; }
+
+        public Point(int a, int b, string name)
+        {
+            this.a = a;
+            this.b = b;
+            this.name = name;
+        }
+    }
+}
