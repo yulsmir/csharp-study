41b33f1dab6a76af11e431046cf902e9869469f1
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-09-12 23:38:17 +0200

    edit: reorganise files

---
+++ 011_OOP/002/Classes/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main()
+        {
+            PartialClass instance = new PartialClass();
+            instance.MethodFromPArt1();
+            instance.MethodFromPArt2();
+
+            Console.ReadKey();
+        }
+    }
+}
