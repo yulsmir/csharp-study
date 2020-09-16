61ab00489353fa35002745ea35943c095612f923
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-09-16 23:07:02 +0200

    add: finish task 01

--- 02_C#_Essential/002_Classes/Task_01/Program.cs
+++ 02_C#_Essential/002_Classes/Task_01/Program.cs
@@ -10,6 +10,9 @@ namespace Task_01
     {
         static void Main(string[] args)
         {
+            User user1 = new User("Login", "John", "Johnson", 25);
+            user1.ShowUserInfo();
+            Console.ReadKey();
         }
     }
 }
--- 02_C#_Essential/002_Classes/Task_01/User.cs
+++ 02_C#_Essential/002_Classes/Task_01/User.cs
@@ -8,8 +8,30 @@ namespace Task_01
 {
     class User
     {
-        string login, Name, lastName;
-        int age;
-        DateTime dateOfFillIn;
+        string login, name, lastName;
+        byte age;
+        readonly DateTime registerDate = new DateTime();
+
+        public string Login { set; get; }
+        public string Name { set; get; }
+        public string LastName { set; get; }
+        public byte Age { set; get; }
+
+        public User(string login, string name, string lastName, byte age)
+        {
+            this.login = login;
+            this.name = name;
+            this.lastName = lastName;
+            this.age = age;
+            this.registerDate = DateTime.Now;
+        }
+        public void ShowUserInfo()
+        {
+            Console.WriteLine("Login: {0}", login);
+            Console.WriteLine("Name: {0}", name);
+            Console.WriteLine("LastName: {0}", lastName);
+            Console.WriteLine("Age: {0}", age);
+            Console.WriteLine("Date of registration: {0}", registerDate);
+        }
     }
 }
