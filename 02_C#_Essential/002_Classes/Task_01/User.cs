a4159681b7edf94f5d62b3f6e9cdf46b77011b5f
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-09-16 22:15:12 +0200

    add: start task 01

---
+++ 02_C#_Essential/002_Classes/Task_01/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_01
+{
+    class User
+    {
+        string login, Name, lastName;
+        int age;
+        DateTime dateOfFillIn;
+    }
+}
