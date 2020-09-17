b5b8582526146d27072261a849d8901f99d0128e
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-09-17 20:37:01 +0200

    edit: finish currency converter

--- 02_C#_Essential/002_Classes/Task_02/Converter.cs
+++ 02_C#_Essential/002_Classes/Task_02/Converter.cs
@@ -10,32 +10,31 @@ namespace Task_02
     {
         double usd, eur, rub;
 
-        public double Usd { set; get; }
-        public double Eur { set; get; }
-        public double Rub { set; get; }
         public Converter(double usd, double eur, double rub)
         {
             this.usd = usd;
-            this.eur = usd;
-            this.rub = usd;
+            this.eur = eur;
+            this.rub = rub;
         }
 
-        public void ConvertToUsd()
+        public void ConvertToUsd(double rubSum)
         {
-            Console.WriteLine("RUB = {0}, USD = {1}: ", rub, usd);
+            Console.WriteLine("RUB = {0}, USD = {1}, RUB to USD: {2}", rub, usd, rubSum / usd);
         }
-        public void ConvertToEur()
+
+        public void ConvertToEur(double rubSum)
         {
-            Console.WriteLine("RUB = {0}, EUR = {1}: ", rub, eur);
+            Console.WriteLine("RUB = {0}, EUR = {1}, RUB to EUR: {2}", rub, eur, rubSum / eur);
         }
-        public void ConvertToRub()
+
+        public void ConvertFromUsd(double usdSum)
         {
-            if (eur)
-            {
-                Console.WriteLine("EUR = {0}, rub = {1}: ", eur, rub);
-            }
-            
-            Console.WriteLine("USD = {0}, rub = {1}: ", usd, rub);
+            Console.WriteLine("USD = {0}, RUB = {1}, USD to RUB: {2}", usd, rub, usdSum / usd);
+        }
+
+        public void ConvertFromEur(double usdSum)
+        {
+            Console.WriteLine("EUR = {0}, RUB = {1}, EUR to RUB: {2}", eur, rub, usdSum / eur);
         }
     }
 }
--- 02_C#_Essential/002_Classes/Task_02/Program.cs
+++ 02_C#_Essential/002_Classes/Task_02/Program.cs
@@ -8,8 +8,16 @@ namespace Task_02
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Converter converter = new Converter(75.30, 80.10, 0.13);
+
+            converter.ConvertToUsd(130);
+            converter.ConvertToEur(800);
+            converter.ConvertFromEur(400);
+            converter.ConvertFromUsd(1000);
+
+            Console.ReadKey();
         }
     }
 }
