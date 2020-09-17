0e5ca99eb7261c9a7ae10bc8827ce02bb6ebac09
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-09-17 21:27:39 +0200

    add: finish task - employee salary calculation

---
+++ 02_C#_Essential/002_Classes/Task_03/Employee.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_03
+{
+    class Employee
+    {
+        string firstname, lastname, jobtitle;
+        double yearsOfExperience;
+        
+
+/*        public string FirstName { get; set; } 
+        public string LastName { get; set; }
+        public string JobTitle { get; set; }
+
+        public double YearsOfExperience { get; set; }*/
+
+        public Employee(string firstname, string lastname, string jobtitle, double yearsOfExperience)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+            this.jobtitle = jobtitle;
+            this.yearsOfExperience = yearsOfExperience;
+        }
+
+        public double getSalary()
+        {
+            double hours = 160D;
+            double hourlyRate = 10D;
+            double salary = (hourlyRate * hours) + (yearsOfExperience * 100);
+
+            if (jobtitle == "manager")
+            {
+                hourlyRate = 8.0D;
+            }
+
+            if (jobtitle == "developer")
+            {
+                hourlyRate = 10.0D;
+            }
+            
+            return salary;
+        }
+
+        public double getTaxes()
+        {
+            double salary = getSalary();
+            double taxes = salary * 0.3;
+            return taxes;
+        }
+
+        public void printEmployeeInfo()
+        {
+            Console.WriteLine("First name: {0}", firstname);
+            Console.WriteLine("Last name: {0}", lastname);
+            Console.WriteLine("Job title: {0}", jobtitle);
+            Console.WriteLine("Salary: {0} USD", getSalary());
+            Console.WriteLine("Taxes: : {0} USD", getTaxes());
+        }
+    }
+}
--- 02_C#_Essential/002_Classes/Task_03/Program.cs
+++ 02_C#_Essential/002_Classes/Task_03/Program.cs
@@ -8,8 +8,21 @@ namespace Task_03
 {
     class Program
     {
-        static void Main(string[] args)
+        static void DrawLine()
         {
+            Console.WriteLine(new string('-', 30));
+        }
+        static void Main()
+        {
+            Employee employee01 = new Employee("Max", "Ivanov", "manager", 4.5);
+            employee01.printEmployeeInfo();
+            DrawLine();
+
+            Employee employee02 = new Employee("John", "Johnsson", "developer", 10);
+            employee02.printEmployeeInfo();
+            DrawLine();
+
+            Console.ReadKey();
         }
     }
 }
