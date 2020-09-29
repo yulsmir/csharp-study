afe5caaf5d02ef46b5058549b5b47ff4625e6fde
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-09-29 03:14:43 +0200

    add: new task - Classroom

---
+++ 02_C#_Essential/003_Classes/Homework/003_ClassRoom/Classes/Pupils/Pupil.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _003_ClassRoom.Classes.Pupils
+{
+    class Pupil
+    {
+        void Study() { }
+        void Read() { }
+        void Write() { }
+        void Relax() { }
+    }
+}
---
+++ 02_C#_Essential/003_Classes/Homework/003_ClassRoom/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _003_ClassRoom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
