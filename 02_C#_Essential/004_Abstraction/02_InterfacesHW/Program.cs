5258f497ed2e510485ff2bc9f9f2f4a1dfa01815
Yuliia Smirnova <julianna.smirnova@gmail.com>
2020-11-06 01:49:44 +0100

    add: finish homework - interfaces - task1 & task 2

--- 02_C#_Essential/004_Abstraction/02_InterfacesHW/Program.cs
+++ 02_C#_Essential/004_Abstraction/02_InterfacesHW/Program.cs
@@ -4,9 +4,50 @@ namespace _02_InterfacesHW
 {
     class Program
     {
+        interface IPlayable
+        {
+            void Play();
+            void Pause();
+            void Stop();
+        }
+
+        interface IRecordable : IPlayable
+        {
+            void Record();
+        }
+
+        class Player : IRecordable
+        {
+            public void Play()
+            {
+                Console.WriteLine("Track is playing");
+            }
+
+            public void Stop()
+            {
+                Console.WriteLine("Track is stopped");
+            }
+
+            public void Pause()
+            {
+                Console.WriteLine("Track is paused");
+            }
+
+            public void Record()
+            {
+                Console.WriteLine("Track is recording");
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Player player = new Player();
+            player.Play();
+            player.Record();
+            player.Stop();
+            player.Pause();
+
+            Console.ReadKey();
         }
     }
 }
